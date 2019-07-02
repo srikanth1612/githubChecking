24004d17a2835cd969da2233b9277d6cab411057
srikanth1612 <50410596+srikanth1612@users.noreply.github.com>
2019-07-02 16:21:57 +0530

    Initial Commit

---
+++ githubChecking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubChecking
+//
+//  Created by MacBook Air on 02/07/19.
+//  Copyright © 2019 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
